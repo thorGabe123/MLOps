bd6bd98a7c76e1479198ed91c5277f29c079e1c2
chphardi <chp.hardi@gmail.com>
2024-01-06 13:36:41 +0100

    Initial data processing

--- project_name/data/make_dataset.py
+++ project_name/data/make_dataset.py
@@ -1,3 +1,30 @@
+import click
+import sqlite3
+import pandas as pd
+
+
+def get_data_from_database(database_dir: str, table: str):
+    """
+    Function that extract data from database
+
+    Args:
+        database_dir (string): directory to the database containing the raw data
+        table (string): The table to extract data from in the database
+
+    Returns:
+        table (DataFrame): DataFrame containing data from the selected table in the database
+    """
+
+    conn = sqlite3.connect(database_dir)
+    query = f'SELECT * FROM {table}'
+
+    table = pd.read_sql_query(query, conn)
+    conn.close()
+
+    return table
+
+
 if __name__ == '__main__':
     # Get the data and process it
-    pass
+    table = get_data_from_database("data/raw/wikibooks.sqlite", "en")
+    table.to_csv('data/processed/data.csv', index=False)

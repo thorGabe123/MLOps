e2707da79880515f485c7e54cf6fa3b4028c08ea
chphardi <chp.hardi@gmail.com>
2024-01-10 20:12:11 +0100

    predict_model for gpt2

--- project_name/predict_model.py
+++ project_name/predict_model.py
@@ -1,17 +1,47 @@
 import torch
+from transformers import GPT2Tokenizer, GPT2LMHeadModel
 
-def predict(
-    model: torch.nn.Module,
-    dataloader: torch.utils.data.DataLoader
-) -> None:
-    """Run prediction for a given model and dataloader.
-    
-    Args:
-        model: model to use for prediction
-        dataloader: dataloader with batches
-    
-    Returns
-        Tensor of shape [N, d] where N is the number of samples and d is the output dimension of the model
 
-    """
-    return torch.cat([model(batch) for batch in dataloader], 0)
+
+def make_prediction(model, tokenizer, text, output_length):
+
+
+    indexed_tokens = tokenizer.encode(text)
+
+    tokens_tensor = torch.tensor([indexed_tokens])
+
+    model.eval()
+
+    # If using GPU
+    # tokens_tensor = tokens_tensor.to('cuda')
+    # model.to('cuda')
+
+    softmax = torch.nn.Softmax(dim=-1)  # Softmax function to convert logits to probabilities
+
+    with torch.no_grad():
+        for i in range(output_length):
+            outputs = model(tokens_tensor)
+            predictions = outputs[0]
+
+            # Apply softmax to convert logits to probabilities
+            probabilities = softmax(predictions[0, -1, :])
+
+            # Get the token with the highest probability
+            predicted_index = torch.argmax(probabilities).item()
+
+            #predicted_probability = torch.max(probabilities).item()
+            #predicted_token = tokenizer.decode(predicted_index)
+            #print(f"Predicted token: {predicted_token}, Probability: {predicted_probability}")
+
+            tokens_tensor = torch.cat((tokens_tensor, torch.tensor([[predicted_index]])), dim=1)
+
+    predicted_text = tokenizer.decode(tokens_tensor[0].tolist())
+    return predicted_text
+
+
+if __name__ == '__main__':
+    tokenizer = GPT2Tokenizer.from_pretrained('gpt2')
+    model = GPT2LMHeadModel.from_pretrained('gpt2')
+    text = "What is the fastest car in the"
+    predicted_text = make_prediction(model, tokenizer, text, 10)
+    print(predicted_text)

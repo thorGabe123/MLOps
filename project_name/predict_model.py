6c390dbf1e7d7816693a95f77f76268b3d618841
chphardi <chp.hardi@gmail.com>
2024-01-11 10:22:46 +0100

    Intial predict file

--- project_name/predict_model.py
+++ project_name/predict_model.py
@@ -1,47 +1,21 @@
 import torch
-from transformers import GPT2Tokenizer, GPT2LMHeadModel
+from transformers import GPT2Tokenizer, GPT2LMHeadModel, GenerationMixin
+import random
 
 
-
-def make_prediction(model, tokenizer, text, output_length):
-
-
-    indexed_tokens = tokenizer.encode(text)
-
+def make_prediction(model, tokenizer, input_prompt, max_output_length):
+    indexed_tokens = tokenizer.encode(input_prompt)
     tokens_tensor = torch.tensor([indexed_tokens])
 
-    model.eval()
+    prediction = model.generate(inputs=tokens_tensor, max_new_tokens=max_output_length)
+    prediction = tokenizer.decode(((prediction.tolist())[0]))
 
-    # If using GPU
-    # tokens_tensor = tokens_tensor.to('cuda')
-    # model.to('cuda')
-
-    softmax = torch.nn.Softmax(dim=-1)  # Softmax function to convert logits to probabilities
-
-    with torch.no_grad():
-        for i in range(output_length):
-            outputs = model(tokens_tensor)
-            predictions = outputs[0]
-
-            # Apply softmax to convert logits to probabilities
-            probabilities = softmax(predictions[0, -1, :])
-
-            # Get the token with the highest probability
-            predicted_index = torch.argmax(probabilities).item()
-
-            #predicted_probability = torch.max(probabilities).item()
-            #predicted_token = tokenizer.decode(predicted_index)
-            #print(f"Predicted token: {predicted_token}, Probability: {predicted_probability}")
-
-            tokens_tensor = torch.cat((tokens_tensor, torch.tensor([[predicted_index]])), dim=1)
-
-    predicted_text = tokenizer.decode(tokens_tensor[0].tolist())
-    return predicted_text
+    return prediction
 
 
 if __name__ == '__main__':
     tokenizer = GPT2Tokenizer.from_pretrained('gpt2')
     model = GPT2LMHeadModel.from_pretrained('gpt2')
-    text = "What is the fastest car in the"
-    predicted_text = make_prediction(model, tokenizer, text, 10)
+    prompt = "What is the fastest car in the"
+    predicted_text = make_prediction(model, tokenizer, prompt, 20)
     print(predicted_text)

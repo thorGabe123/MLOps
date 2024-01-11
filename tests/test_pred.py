02d3eff30e2b86042f76648bf97aeec97c9dafbe
chphardi <chp.hardi@gmail.com>
2024-01-11 15:39:10 +0100

    Test of prediction changed

--- tests/test_pred.py
+++ tests/test_pred.py
@@ -1,5 +1,6 @@
 from project_name.predict_model import make_prediction
-from transformers import GPT2Tokenizer, GPT2LMHeadModel
+from project_name.models.model import Model
+
 
 def test_pred():
     """
@@ -7,9 +8,8 @@ def test_pred():
     that it can make predictions given a tokenizer, model and an input prompt.
     """
 
-    tokenizer = GPT2Tokenizer.from_pretrained('gpt2')
-    model = GPT2LMHeadModel.from_pretrained('gpt2')
+    model = Model(model_version='gpt2')
     prompt = "How can predictions be tested"
-    predicted_text = make_prediction(model, tokenizer, prompt, 20)
+    predicted_text = make_prediction(model, prompt, 20)
 
     assert len(predicted_text) > len(prompt), "No prediction added to the prompt"

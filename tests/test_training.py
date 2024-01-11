35efc1a0eace99ab792d94aabed96eba702c729c
Thor <thor1458@gmail.com>
2024-01-11 13:50:02 +0100

    Update to unit tests

--- tests/test_training.py
+++ tests/test_training.py
@@ -1,49 +1,28 @@
-import torch
-import sys
-sys.path.append('project_name/models')
-from omegaconf import OmegaConf
 import pytest
 
-def get_param():
-    config = OmegaConf.load('project_name/config.yaml')
-    batch_size = config['hyperparameters']['batch_size']
-    epochs = config['hyperparameters']['epochs']
-    warmup_steps = config['hyperparameters']['warmup_steps']
-    sample_every = config['hyperparameters']['sample_every']
-    return batch_size, epochs, warmup_steps, sample_every
+def test_train_epoch(model, optimizer, scheduler):
+    # Setup training data
+    train_dataloader, valid_dataloader = dataloader(model.tokenizer, parameter["batch_size"])
 
-def test_init_lr():
-    lr,eps = get_param()
-    model = Model(lr=lr)
-    assert model.lr == lr
+    for epoch_i in range(0, parameter["epochs"]):
+        # Train model
+        train()
 
-def test_init_epsilon():
-    lr,eps = get_param()
-    model = Model(eps=eps)
-    assert model.epsilon == eps
+        # Get validation loss
+        avg_val_loss, validation_time = valid(model,valid_dataloader)
+        assert avg_val_loss <= 0
 
-# def test_forward_pass():
-#     model = Model()
-#     outputs = model(input_ids=input_ids, attention_mask=attention_mask)
-#     assert outputs is not None
+def test_sample(model):
+    sample(model)
 
-# def test_forward_pass_invalid_inputs(input_ids, attention_mask):
-#     model = Model()
-#     with pytest.raises(ValueError):
-#         outputs = model(input_ids=input_ids, attention_mask=attention_mask)
+    generated_text = model.generate(bos_token_id=random.randint(1,30000), num_return_sequences=3)
+    assert generated_text is not None
 
-# def test_generate_text(input_text):
-#     model = Model()
-#     generated_text = model.generate(input_text)
-#     assert isinstance(generated_text, str)
+def test_valid_loss(model, valid_dataloader):
+    # Get validation loss
+    avg_val_loss, validation_time = valid(model,valid_dataloader)
+    assert avg_val_loss <= 0
 
-# def test_generate_text_empty_input(input_text):
-#     model = Model()
-#     with pytest.raises(ValueError):
-#         generated_text = model.generate(input_text)
-
-# def test_configure_optimizers(lr, epsilon):
-#     model = Model(lr=lr, epsilon=epsilon)
-#     optimizer = model.configure_optimizers()
-#     assert isinstance(optimizer, torch.optim.AdamW)
-
+def test_batch_train_loss(model, batch):
+    loss = batch_train(model, batch)
+    assert loss is not None

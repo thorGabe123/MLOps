ccfdb0469b3d1f5e0b4da7e8eeccc0118d83cf3c
chphardi <chp.hardi@gmail.com>
2024-01-11 13:32:17 +0100

    Slight adjustment to predict

--- project_name/predict_model.py
+++ project_name/predict_model.py
@@ -3,12 +3,12 @@ from models.model import Model
 
 
 
-def make_prediction(model, tokenizer, input_prompt, max_output_length):
+def make_prediction(model, input_prompt, max_output_length):
     indexed_tokens = tokenizer.encode(input_prompt)
     tokens_tensor = torch.tensor([indexed_tokens])
     prediction = model.generate(inputs=tokens_tensor, 
-                                max_output_length = max_output_length,
-                                num_return_sequences = 1)
+                                max_output_length=max_output_length,
+                                num_return_sequences=1)
 
     return prediction
 
@@ -17,5 +17,5 @@ if __name__ == '__main__':
     model = Model()
     tokenizer = model.tokenizer
     prompt = "What is the fastest car in the"
-    predicted_text = make_prediction(model, tokenizer, prompt, 200)
+    predicted_text = make_prediction(model, prompt, 200)
     print(predicted_text[0])

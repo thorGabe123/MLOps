ae64fa30f93cc081a6613feefaf9594bd3eda84b
Ting-Hui Cheng鄭婷卉 <tinghui1738cheng@gmail.com>
2024-01-11 13:02:22 +0100

    Small change for prediction

--- project_name/predict_model.py
+++ project_name/predict_model.py
@@ -1,14 +1,14 @@
 import torch
-from transformers import GPT2Tokenizer, GPT2LMHeadModel
 from models.model import Model
-import random
 
 
 
 def make_prediction(model, tokenizer, input_prompt, max_output_length):
     indexed_tokens = tokenizer.encode(input_prompt)
     tokens_tensor = torch.tensor([indexed_tokens])
-    prediction = model.generate(inputs=tokens_tensor)
+    prediction = model.generate(inputs=tokens_tensor, 
+                                max_output_length = max_output_length,
+                                num_return_sequences = 1)
 
     return prediction
 
@@ -17,5 +17,5 @@ if __name__ == '__main__':
     model = Model()
     tokenizer = model.tokenizer
     prompt = "What is the fastest car in the"
-    predicted_text = make_prediction(model, tokenizer, prompt, 20)
-    print(predicted_text)
+    predicted_text = make_prediction(model, tokenizer, prompt, 200)
+    print(predicted_text[0])
